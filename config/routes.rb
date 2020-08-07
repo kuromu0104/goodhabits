4894fc34cfe7b2ce02c582ef70af6e2c076ec138
kuromu0104 <nccjb457@yahoo.co.jp>
2020-08-07 15:04:12 +0900

    投稿ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   devise_scope:user do
    get 'login', to:'devise/sessions#new'
   end
+
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'habit'
+      get 'team'
+    end
+  end
 end
