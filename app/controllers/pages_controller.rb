94b31f8887ec011ffe8de8a62c033fedfbd1e378
kuromu0104 <nccjb457@yahoo.co.jp>
2020-08-10 11:12:07 +0900

    ポップアップウィンドウ追加
    modal.js追加
    modal.scss追加
    post要素にbootstrap追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-
   def index
     @posts = Post.limit(5)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
       get 'team'
     end
   end
+  
 end
